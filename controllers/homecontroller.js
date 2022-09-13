1c95cbac251dc690ab4331be4101df31963da238
xSPM <mahersean94@gmail.com>
2022-09-13 12:54:59 +1000

    Added side navigation bar for parts
    Added a side navigation bar for pc parts and added the routing for the respective pages

--- controllers/homecontroller.js
+++ controllers/homecontroller.js
@@ -10,8 +10,33 @@ const accountCreated = (req,res) => {
     res.render('accountCreated')
 }
 
+const cpu = (req,res) => {
+    res.render('cpu')
+}
+
+const gpu = (req,res) => {
+    res.render('gpu')
+}
+
+const ram = (req,res) => {
+    res.render('ram')
+}
+
+const motherboard = (req,res) => {
+    res.render('motherboard')
+}
+
+const powerSupply = (req,res) => {
+    res.render('powerSupply')
+}
+
 module.exports = {
     home,
     createAccount,
     accountCreated,
+    cpu,
+    gpu,
+    ram,
+    motherboard,
+    powerSupply,
 }
--- routes/homerouter.js
+++ routes/homerouter.js
@@ -5,5 +5,10 @@ const homeController = require('../controllers/homeController.js')
 homeRouter.get('/', homeController.home)
 homeRouter.get('/createAccount', homeController.createAccount)
 homeRouter.get('/accountCreated', homeController.accountCreated)
+homeRouter.get('/cpu', homeController.cpu)
+homeRouter.get('/gpu', homeController.gpu)
+homeRouter.get('/motherboard', homeController.motherboard)
+homeRouter.get('/powerSupply', homeController.powerSupply)
+homeRouter.get('/ram', homeController.ram)
 
 module.exports = homeRouter
