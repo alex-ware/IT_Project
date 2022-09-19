fb41ac645e00a159e5e21419c7a206db5f178faa
xSPM <mahersean94@gmail.com>
2022-09-19 17:36:13 +1000

    added routing for wishlist and history pages, also added html layout for all part, history and wishlist pages
    Added routing for the wishlist and history pages for the users page, also added some html for the user home page, part's pages, wishlist and history page. The html is a placeholder for database content, but the basic structure for these pages are available. CSS will be required

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -8,6 +8,27 @@ user_router.use(bodyParser.urlencoded({extended: true}));
 user_router.get('/', function(req, res) {
     res.render('userHomepage', { email: req.user.username, layout: 'user.hbs' })
 })
+user_router.get('/gpu', function(req, res) {
+    res.render('gpu', {layout: 'user.hbs' })
+})
+user_router.get('/cpu', function(req, res) {
+    res.render('cpu', {layout: 'user.hbs' })
+})
+user_router.get('/motherboard', function(req, res) {
+    res.render('motherboard', {layout: 'user.hbs' })
+})
+user_router.get('/ram', function(req, res) {
+    res.render('ram', {layout: 'user.hbs' })
+})
+user_router.get('/powerSupply', function(req, res) {
+    res.render('powerSupply', {layout: 'user.hbs' })
+})
+user_router.get('/wishlist', function(req, res) {
+    res.render('wishlist', {layout: 'user.hbs' })
+})
+user_router.get('/userHistory', function(req, res) {
+    res.render('userHistory', {layout: 'user.hbs' })
+})
 
 // Handle logout
 user_router.post('/logout', (req, res) => {
